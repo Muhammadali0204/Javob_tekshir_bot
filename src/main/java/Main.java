73cb838959cd9d653ce0511bb0004b8d11acb2e5
Muhammadali0204 <mamadjonovmuhammadali7@gmail.com>
2022-08-17 01:42:04 +0500

    make it better

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String [] args){
